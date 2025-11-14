c953b531c9d596ddb030ff3798aa2f0c31e9bd24
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-14 21:12:53 +0530

    #Constructor Time=>

--- Lec04/Gama.ts
+++ Lec04/Gama.ts
@@ -8,3 +8,29 @@ let S2 = new Student();
 console.log(S1)
 console.log(S2)
 
+// Constructor
+
+// Example => 1
+
+class Humans{
+    age=0
+    constructor(public name:string, public Year:number){
+
+    }
+}
+let obj = new Humans("Pranjal",2005)
+console.log(obj)
+let a = new Humans("Harsh",2006)
+console.log(a)
+
+// Example=>2
+
+class BottleMaker{
+    constructor(public brand:string,public price:number,public color:string){
+
+    }
+}
+let obj2 =  new BottleMaker("Milton",1200,"white")
+console.log(obj2)
+let obj1 =  new BottleMaker("Cello",1300,"blue")
+console.log(obj1)
