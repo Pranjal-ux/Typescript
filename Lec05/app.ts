9e3dc70acb0e1e5b33c42a8e2cb652c9f9ec7e68
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-16 10:20:43 +0530

    #NodeModules

--- Lec05/app.ts
+++ Lec05/app.ts
@@ -2,63 +2,78 @@
 
 //public=> HAR JAGHA USE KAR SAKTE HAI CLASS ME BHI AUR KISI DUSRI CLASS ME BHI
 
-class BottleMaker{
-    constructor(public name:string){
+// class BottleMaker{
+//     constructor(public name:string){
        
 
-    }
-}
-class MetalBottleMaker extends BottleMaker{
-    constructor(name:string){
-        super(name)
+//     }
+// }
+// class MetalBottleMaker extends BottleMaker{
+//     constructor(name:string){
+//         super(name)
 
-    }
-    getValue(){
-        console.log(this.name)
-    }
-}
-let obj1 = new MetalBottleMaker("LOHA")
-// console.log(obj)
-obj1.getValue()
+//     }
+//     getValue(){
+//         console.log(this.name)
+//     }
+// }
+// let obj1 = new MetalBottleMaker("LOHA")
+//  console.log(obj)
+// obj1.getValue()
 
 // PRIVATE => JO CLASS PRIVATE HAI USSI ME HUM ACCESS KAR SAKTE HAI BAKKI KAHI NHI KAR SAKTE HAI
 
-class BottleMakerS{
-    constructor(private name:string){
+// class BottleMakerS{
+//     constructor(private name:string){
 
-    }
+//     }
    
-}
-class MetalBottleMakerS extends BottleMakerS{
-    constructor(name:string){
-        super(name)
+// }
+// class MetalBottleMakerS extends BottleMakerS{
+//     constructor(name:string){
+//         super(name)
 
-    }
-    getValueS(){
-        console.log(this.name)
-    }
-}
-let obj = new MetalBottleMakerS("LOHA")
-// console.log(obj)
-obj.getValueS()
+//     }
+//     getValueS(){
+//         console.log(this.name)
+//     }
+// }
+// let obj = new MetalBottleMakerS("LOHA")
+//  console.log(obj)
+// obj.getValueS()
 
 
 ///PROTECTED => JO CLASS PROTECTED HAI USME USE KAR SAKTE HAI AUR EXTENDED CLASS ME USE KAR SAKTE HAI
 
-class WaterBottle{
-    protected name = "AppleBottle"
-}
+// class WaterBottle{
+//     protected name = "AppleBottle"
+// }
 
 
-class LohaBody extends WaterBottle{
-public material = "metal"
+// class LohaBody extends WaterBottle{
+// public material = "metal"
 
-changeName(){
-    this.name = "Cello"
-    console.log(this.name)
+// changeName(){
+//     this.name = "Cello"
+//     console.log(this.name)
     
     
+// }
+// }
+// let obj3 = new LohaBody();
+// obj3.changeName()
+
+//READONLY PRROPERTY
+
+class Animal{
+    constructor(public name:string){
+
+    }
+    changeName(){
+        this.name = "Sherrr"
+    }
 }
-}
-let obj3 = new LohaBody();
-obj3.changeName()
+let a = new Animal("chittta")
+console.log(a)
+a.changeName()
+console.log(a)
