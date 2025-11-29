3761a21a6f105c946535f1aa1906c79f160a6de6
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-29 14:00:02 +0530

    #Changes

---
+++ Practise/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Q1=> What is ts?
+// TypeScript (TS) is JavaScript with types — it catches errors before running your code and makes development safer and easier.
+//Array
+let name = ["ben", "kevin", "carnaige"];
+console.log(name);
+//# sourceMappingURL=app.js.map
---
+++ Practise/app.ts
@@ -0,0 +1,17 @@
+// Q1=> What is ts?
+// TypeScript (TS) is JavaScript with types — it catches errors before running your code and makes development safer and easier.
+//Array
+let name:string[]=["ben","kevin","carnaige"]
+console.log(name[0])
+
+let emplName :Array<string> = ["ben","kevin","carnaige"]
+let emplid :Array<number> = [1,2,3,45,6,6]
+
+//multi arr
+let arr:(string|number|boolean)[] =["pranjla",2,true]
+//First arr decleration
+// let a:Array<string>
+// a = ["om pathak","bhosdiwala","hai"]
+// console.log(a)
+
+//1 
